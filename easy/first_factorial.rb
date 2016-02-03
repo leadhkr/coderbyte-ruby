ce83f465fa9a6fe95fbd2426eed9cf49a17c16af
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-02-03 09:54:47 -0500

    Complete First Factorial

---
+++ easy/first_factorial.rb
@@ -0,0 +1,16 @@
+require 'pry'
+def first_factorial(num)
+  # Easy Way
+  (1..num).inject {|total, num| total *= num}
+
+  # Without Inject Method
+
+  # (1..num).inject {|total, num| total *= num}
+  # total = 1
+  # while num != 0
+  #   total *= num
+  #   num -= 1
+  # end
+  # return total
+
+end
